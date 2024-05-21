fe34f251b851ef9512722052bbc9ab520d7ce40a
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-21 17:29:00 +0500

    routing

--- 06/server.js
+++ 06/server.js
@@ -1,21 +1,27 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
 
 const server =  http.createServer((req, res) => {
 	switch (req.url) {
-		case '/students':
-			setTimeout(() => {
-				const data = 'STUDENTS '
-				res.write(data);
-				res.end();
-			}, 3000);
+		case '/about': {
+			const data = fs.readFileSync('pages/about.html');
+			res.write(data);
+			res.end();
 			break;
-		default:
+		}
+		case '/home': {
+			const data = fs.readFileSync('pages/home.html');
+			res.write(data);
+			res.end();
+			break;
+		}
+		default: {
 			res.write('ERROR ');
 			res.end();
+		}
 	}
-
 })
 
 server.listen(3004);
