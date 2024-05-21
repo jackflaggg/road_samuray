70d5074cee5cfb592d5e4942a42044021ef935b1
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-21 16:21:02 +0500

    configure server

--- 05/server.js
+++ 05/server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const http = require('http');
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
 	res.write('it kamasutra')
 	res.end();
 })
-server.listen(3003);
+server.listen(port);
