9963eafc63275ade35cfeb95fafe78d6effcb9d2
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-21 16:37:02 +0500

    routing

--- 05/server.js
+++ 05/server.js
@@ -7,7 +7,19 @@ let counter = 0;
 const server = http.createServer((req, res) => {
 	if (req.url !== 'favicon.ico'){
 		counter++;
-		res.write('it kamasutra: ' + counter)
+		console.log(req.url);
+
+		switch (req.url) {
+			case '/students':
+				console.log(req.url === '/students');
+				res.write('STUDENTS ');
+				break;
+			case '/courses':
+				res.write('COURSES ')
+				break;
+			default:
+				res.write('ERROR ');
+		}
 	}
 	res.end();
 })
