4a34b4a1fe72d9e6ad8e3035efab68279a49bc9e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-21 17:16:53 +0500

    routing

--- 06/server.js
+++ 06/server.js
@@ -6,6 +6,12 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
 	switch (req.url) {
 		case '/students':
+			const data= 'best';
+			const start = new Date();
+
+			while (new Date() - start <= 5000) {
+				console.log(new Date() - start)
+			}
 			res.write('STUDENTS ');
 			break;
 		case '/courses':
