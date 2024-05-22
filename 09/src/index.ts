0fe4beed5f5153aa9c6378063cca1af1aaed7b15
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-22 20:28:33 +0500

    init express po

--- 09/src/index.ts
+++ 09/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 8080;
+const port: string | number = process.env.PORT || 3015;
 
 
 app.get('/', (req, res) => {
-
-})
+    let chunk = 'hello';
+    res.send(chunk);
+});
 
 app.listen(port, () => {
-    console.log(`APP started on port: ${port}}`)
-})
+    console.log(`APP started on port: ${port}`)
+});
