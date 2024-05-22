f1b982ab6735d50cdf29b958c0af3e6f8f519249
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-22 20:34:30 +0500

    init express po

--- 09/src/index.ts
+++ 09/src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 const app = express();
 const port: string | number = process.env.PORT || 3015;
 
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     let chunk = 'hello';
     res.send(chunk);
 });
