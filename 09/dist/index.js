53ac2d7f464c1c909fd9fc829bae8e5dc530664f
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-22 22:31:22 +0500

    init express po

--- 09/dist/index.js
+++ 09/dist/index.js
@@ -6,9 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3015;
-app.get('/', (req, res) => {
-    let chunk = 'hello';
+const dataProducts = [{ title: 'tomato' }, { title: 'apple' }];
+const dataAddresses = [{ value: 'Lenina 30' }, { value: 'Artema 128' }];
+app.get('/products', (req, res) => {
+    res.send(dataProducts);
+});
+app.get('/products/:id', (req, res) => {
+    let chunk = dataProducts.find(product => product.title === 'apple');
     res.send(chunk);
+});
+app.get('/addresses', (req, res) => {
+    res.send(dataAddresses);
 });
 app.listen(port, () => {
     console.log(`APP started on port: ${port}`);
--- 09/src/index.ts
+++ 09/src/index.ts
@@ -1,13 +1,23 @@
 import express, {Request, Response} from 'express';
+
 const app = express();
 const port: string | number = process.env.PORT || 3015;
 
+const dataProducts = [{title: 'tomato'}, {title: 'apple'}];
+const dataAddresses = [{value: 'Lenina 30'}, {value: 'Artema 128'}]
 
-app.get('/', (req: Request, res: Response) => {
-    let chunk = 'hello';
+app.get('/products', (req: Request, res: Response) => {
+    res.send(dataProducts);
+});
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    let chunk = dataProducts.find(product => product.title === 'apple');
     res.send(chunk);
 });
 
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(dataAddresses);
+});
 app.listen(port, () => {
     console.log(`APP started on port: ${port}`)
 });
